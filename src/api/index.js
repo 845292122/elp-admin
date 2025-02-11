c305f10dec7ec473d02a2350900230dc023ee289
edison <845292122@qq.com>
2025-02-11 22:53:37 +0800

    feat: 新增api

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,4 @@
 export * from './auth'
 
 // * system
-// export * from './system/role'
-// export * from './system/user'
-// export * from './system/tenant'
-// export * from './system/permission'
-export * from './system/account'
+export * from './tenant.api'
