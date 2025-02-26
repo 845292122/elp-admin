8ddaf943ff80e15ec1d1ffc54a7f63fb207f24da
edison <845292122@qq.com>
2025-02-26 21:37:32 +0800

    添加表单

--- src/views/system/tenant/index.vue
+++ src/views/system/tenant/index.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { TenantApi } from '@/api'
 
-const tableColumn = [
+const tableColumns = [
   {
     label: 'ID',
     tooltip: '租户ID',
@@ -121,28 +121,135 @@ const tableColumn = [
     hideInSearch: true
   }
 ]
-const state = reactive({
-  query: {
-    contactName: undefined,
-    isPremium: undefined
+const drawerFormRules = {}
+const drawerFormColumns = [
+  {
+    label: 'ID',
+    width: 100,
+    prop: 'id'
   },
-  selectIds: []
+  {
+    label: '联系人',
+    width: 120,
+    prop: 'contactName'
+  },
+  {
+    label: '手机号',
+    width: 120,
+    prop: 'contactPhone'
+  },
+  {
+    label: '公司名称',
+    width: 120,
+    prop: 'companyName'
+  },
+  {
+    label: '统一信用代码',
+    width: 120,
+    prop: 'licenseNumber'
+  },
+  {
+    label: '地址',
+    width: 120,
+    prop: 'address'
+  },
+  {
+    label: '域名',
+    width: 120,
+    prop: 'domain'
+  },
+  {
+    label: '备注',
+    width: 120,
+    prop: 'remark'
+  },
+  {
+    label: 'premium',
+    width: 120,
+    prop: 'isPremium',
+    valueType: 'switch'
+  },
+  {
+    label: '状态',
+    width: 100,
+    prop: 'status',
+    valueType: 'select',
+    options: [
+      {
+        label: '未开始',
+        value: '0',
+        color: 'gray'
+      },
+      {
+        label: '试用中',
+        value: '1',
+        color: 'blue'
+      },
+      {
+        label: '试用结束',
+        value: '2',
+        color: 'yellow'
+      },
+      {
+        label: '使用中',
+        value: '3',
+        color: 'blue'
+      },
+      {
+        label: '已过期',
+        value: '4',
+        color: 'red'
+      }
+    ]
+  },
+  {
+    label: '试用开始时间',
+    width: 200,
+    prop: 'trialStartDate',
+    valueType: 'date-picker'
+  },
+  {
+    label: '试用结束时间',
+    width: 200,
+    prop: 'trialEndDate',
+    valueType: 'date-picker'
+  },
+  {
+    label: '订阅开始时间',
+    width: 120,
+    prop: 'startDate',
+    valueType: 'date-picker'
+  },
+  {
+    label: '订阅结束时间',
+    width: 120,
+    prop: 'endDate',
+    valueType: 'date-picker'
+  }
+]
+
+const drawerFormVisible = ref(false)
+const drawerFormValue = ref({})
+const tableQuery = ref({
+  contactName: undefined,
+  isPremium: undefined
 })
 
 async function loadRecords(params) {
   const { records, total } = await TenantApi.page(params)
   return { data: records, total }
 }
-
 async function handleBatchDelete() {}
-async function handleCreate() {}
+async function handleCreate() {
+  drawerFormVisible.value = true
+}
 </script>
 
 <template>
   <PlusPage
     ref="plusPageInstance"
-    :columns="tableColumn"
-    :params="state.query"
+    :columns="tableColumns"
+    :params="tableQuery"
     :request="loadRecords"
     :search="{ labelWidth: '80px', colProps: { span: 8 } }"
     :table="{ isSelection: true, onSelectionChange: handleSelect }"
@@ -150,8 +257,15 @@ async function handleCreate() {}
     <template #table-title>
       <el-row class="button-row">
         <el-button type="primary" @click="handleCreate"> 添加 </el-button>
-        <el-button type="danger" @click="handleBatchDelete"> 批量删除 </el-button>
+        <el-button type="danger" @click="handleBatchDelete" disabled> 批量删除 </el-button>
       </el-row>
     </template>
   </PlusPage>
+
+  <PlusDrawerForm
+    title="租户信息"
+    v-model:visible="drawerFormVisible"
+    v-model="drawerFormValue"
+    :form="{ columns: drawerFormColumns, drawerFormRules }"
+  />
 </template>
