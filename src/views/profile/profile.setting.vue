dae3b2c19116dc1fb0848f37b122994d55123e78
edison <845292122@qq.com>
2025-04-04 22:01:45 +0800

    feat: secure page

--- src/views/profile/profile.setting.vue
+++ src/views/profile/profile.setting.vue
@@ -5,10 +5,47 @@ import { useAuthStore } from '@/store'
 const activeTab = ref('info')
 const authStore = useAuthStore()
 const { info } = authStore
+const infoFormRef = ref()
+
+const secureForm = ref({
+  oldPassword: undefined,
+  newPassword: undefined,
+  confirmPassword: undefined
+})
 
 const infoForm = ref({
   phone: undefined
 })
+
+const infoFormRules = reactive({
+  phone: [
+    { required: true, message: '请输入手机号', trigger: 'blur' },
+    {
+      pattern: /^1[3-9]\d{9}$/,
+      message: '请输入正确的手机号',
+      trigger: 'blur'
+    }
+  ],
+  nickname: [{ required: true, message: '请输入名称', trigger: 'blur' }],
+  email: [
+    {
+      pattern: /^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/,
+      message: '请输入正确的邮箱',
+      trigger: 'blur'
+    }
+  ]
+})
+
+async function updateInfo() {
+  await ElForm.validateField(infoForm, 'infoForm')
+  await authStore.updateInfo(infoForm.value)
+  ElMessage({
+    type: 'success',
+    message: '修改成功',
+    plain: true
+  })
+}
+
 function initInfo() {
   infoForm.value = cloneDeep(info)
 }
@@ -24,7 +61,7 @@ initInfo()
     </template>
     <el-tabs v-model="activeTab">
       <el-tab-pane label="基本信息" name="info">
-        <el-form :model="infoForm" label-width="auto">
+        <el-form :model="infoForm" label-width="auto" :rules="infoFormRules" ref="infoFormRef">
           <el-form-item label="名称" prop="nickname">
             <el-input v-model="infoForm.nickname" />
           </el-form-item>
@@ -35,9 +72,22 @@ initInfo()
             <el-input v-model="infoForm.email" />
           </el-form-item>
         </el-form>
-        <el-button type="primary"> 更新信息 </el-button>
+        <el-button type="primary" @click="updateInfo"> 更新信息 </el-button>
       </el-tab-pane>
-      <el-tab-pane label="安全设置" name="secure">安全设置</el-tab-pane>
+      <el-tab-pane label="安全设置" name="secure">
+        <el-form :model="secureForm" label-width="auto">
+          <el-form-item label="旧密码" prop="oldPassword">
+            <el-input v-model="secureForm.oldPassword" />
+          </el-form-item>
+          <el-form-item label="新密码" prop="newPassword">
+            <el-input v-model="secureForm.newPassword" />
+          </el-form-item>
+          <el-form-item label="确认密码" prop="confirmPassword">
+            <el-input v-model="secureForm.confirmPassword" />
+          </el-form-item>
+        </el-form>
+        <el-button type="primary"> 更新密码 </el-button>
+      </el-tab-pane>
     </el-tabs>
   </el-card>
 </template>
